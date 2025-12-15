2dc3d1e83e9021ca3aa259f478f6e5bd1ecc2284
tinzack <altinsylejmani2003@gmail.com>
2025-12-15 16:33:24 +0100

    first commit

---
+++ chat.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+var burger int8=100
+var toast  int8=100
+var cola int8=100
+var fanta int8=100
+
+
+
+func proccessOrder(p int) int{
+
+	return 0
+}
+
+
+func main(){
+var foodOrder int
+var foodAmount int 
+fmt.Println("Welcome to :Original Taste")
+
+fmt.Printf("Order menu: \n Foods: 1.Burger 2.Toast \n Drinks: 1:Cola 2.Fanta")
+fmt.Scan(&foodOrder)
+
+if foodOrder
+
+
+
+
+
+
+
+}
