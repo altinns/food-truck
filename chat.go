67d892480cb6390659001087d980973f99ad462b
altin <117034839+altinsylejmani@users.noreply.github.com>
2025-12-15 22:52:59 +0100

    small update

--- chat.go
+++ chat.go
@@ -1,33 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-var burger int8=100
-var toast  int8=100
-var cola int8=100
-var fanta int8=100
+var Foodlist=map[string]int{
 
-
-
-func proccessOrder(p int) int{
-
-	return 0
+	"burger":100,
+"toast":100,
+"Cola":100,
+"Fanta":100,
 }
 
 
+
+func  FdOrder(x string,a int)int{
+ amount:=a
+result:=Foodlist[x]
+
+	return result-amount
+}
+
+
+
+
+
 func main(){
-var foodOrder int
+
+var foodOrder string
 var foodAmount int 
-fmt.Println("Welcome to :Original Taste")
-
-fmt.Printf("Order menu: \n Foods: 1.Burger 2.Toast \n Drinks: 1:Cola 2.Fanta")
+//var drinkOrder int
+ //var drinkAmount int 
+ 
+fmt.Printf("Welcome to :Original Taste")
+fmt.Printf("Order menu: \n Foods: 1.Burger 2.Toast \n")
 fmt.Scan(&foodOrder)
+fmt.Printf("How many?")
+fmt.Scan(&foodAmount)
+//fmt.Println("Drinks: 1.Cola 2.Fanta")
+//fmt.Scan(&drinkOrder)
+//fmt.Printf("How many?")
+//fmt.Scan(&drinkAmount)
+//fmt.Println(Foodlist)
 
-if foodOrder
+result:=FdOrder(foodOrder,foodAmount)
 
-
-
-
+fmt.Println(result)
 
 
 
